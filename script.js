c5ca151875b82ec8f9254e7cb23df0a14ae2c917
Levi <leviamullins@gmail.com>
2026-06-08 01:41:45 -0400

    new features

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const canvas = document.getElementById("posterCanvas");
 const ctx = canvas.getContext("2d");
 const guess = document.getElementById("guess");
-const nxtbtn = document.getElementById("next");
+const header1 = document.querySelector("h1");
+const header2 = document.querySelectorAll("h2");
 const buttonArray = [];
 const btn1 = document.getElementById("1");
 const btn2 = document.getElementById("2");
@@ -11,7 +12,9 @@ const btn5 = document.getElementById("5");
 const movies = new Map();
 let storedString = "";
 let guessCount = 1;
+let fauxguessCount = 1; // used for btns 1-5
 let pixelationScale = 3;
+const pixelationReview = [3, 5, 7, 9, 11, 120];
 
 const img = new Image();
 img.crossOrigin = "anonymous";
@@ -61,7 +64,15 @@ function pixelate(img, factor) {
 guess.addEventListener('keydown', (event) => {
     if(event.key === 'Enter') {
         if(guessCount > 5) {
+            fauxguessCount = 10;
             pixelate(img, 120);
+            createNextButton(); 
+            guess.remove();
+            header1.textContent = "well... you tried i guess, hit next to try another one.";
+            header1.style.cssText = "padding-bottom: 40px;"
+            for(const head of header2) {
+                head.remove();
+            }
             return;
         }
         event.preventDefault();
@@ -69,27 +80,77 @@ guess.addEventListener('keydown', (event) => {
         storedString = storedString.toLowerCase().trim(); // normalize user input
         if(storedString === correctTitle) {
             pixelate(img, 120);
+            fauxguessCount = 10;
             buttonArray[guessCount].style.cssText = "background-color: green;";
-
+            createNextButton();
+            guess.remove();
+            header1.textContent = "Congrats! You got it, hit next to try another one.";
+            header1.style.cssText = "padding-bottom: 40px;"
+            for(const head of header2) {
+                head.remove();
+            }
         } else if(storedString !== correctTitle && guessCount <= 5){
             pixelationScale += 2;
             pixelate(img, pixelationScale);
             buttonArray[guessCount].style.cssText = "background-color: red;";
             guessCount++;
+            fauxguessCount++;
         }
         guess.value = "";
     }
 });
-
-nxtbtn.addEventListener('click', () => {
-    location.reload();
-})
 
 function populateButtonArray() {
     for(let i = 1; i <= 5; i++) {
         buttonArray[i] = document.getElementById(i.toString());
     }
 }
+
+function createNextButton() {
+    const nxtbtn = document.createElement('button');
+    nxtbtn.innerText = 'next';
+    nxtbtn.type = 'button';
+    nxtbtn.id = 'next';
+    nxtbtn.addEventListener('click', function() {
+       location.reload(); 
+    });
+
+    document.querySelector('.guess-section').appendChild(nxtbtn);
+}
+
+btn1.addEventListener('click', function() {
+    if(fauxguessCount >= 1)
+        pixelate(img, pixelationReview[0]); 
+});
+
+btn2.addEventListener('click', function() {
+    if(fauxguessCount >= 2) {
+        pixelate(img, pixelationReview[1]);
+    }
+});
+
+btn3.addEventListener('click', function() {
+    if(fauxguessCount >= 3) {
+        pixelate(img, pixelationReview[2]);
+    }
+});
+
+btn4.addEventListener('click', function() {
+    if(fauxguessCount >= 4) {
+        pixelate(img, pixelationReview[3]);
+    }
+});
+
+btn5.addEventListener('click', function() {
+    if(fauxguessCount === 10) {
+        pixelate(img, pixelationReview[5]); // used so that after answer is revealed, user can see the full picture when pressing 5.
+    } else if(fauxguessCount >= 5) {
+        pixelate(img, pixelationReview[4]); // used so that before answer is revealed user sees the 5th pixelated view.
+    }
+});
+
+// plans from here on out: create a marathon mode where we keep track of how many the user got right in a single run
+// also if there is a more consistent way to prevent the same posters from coming up again and again that would be a good idea.
 
 populateButtonArray();
 generateMovieForTheDay();
